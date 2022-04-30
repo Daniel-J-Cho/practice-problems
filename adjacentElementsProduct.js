ef377d905a855ab78f433c51192ada0422e738d5
Daniel Cho <danielcho07@gmail.com>
2022-04-30 19:08:30 +0000

    adjacentElementsProduct solution from CodeSignal

---
+++ adjacentElementsProduct.js
@@ -0,0 +1,9 @@
+function solution(inputArray) {
+  var products = [];
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    products.push(inputArray[i] * inputArray[i + 1]);
+  }
+  return Math.max(...products);
+}
+
+solution();
