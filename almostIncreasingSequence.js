421bad1add7480bf64da2fc007ae88c4b03e994c
Daniel Cho <danielcho07@gmail.com>
2022-05-15 17:40:05 +0000

    almostIncreasingSequence solution from codeSignal.

---
+++ almostIncreasingSequence.js
@@ -0,0 +1,15 @@
+function solution(sequence) {
+  let lessThanCount = 0;
+  for (let i = 1; i < sequence.length; i++) {
+    if (sequence[i] <= sequence[i - 1]) {
+      lessThanCount++;
+    } if (lessThanCount > 1) {
+      return false;
+    } if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+solution();
