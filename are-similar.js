500501daf2385ab57c969a0df17a283a009d5d06
Daniel Cho <danielcho07@gmail.com>
2022-08-26 22:49:11 +0000

    Better solution for areSimilar from codeSignal.

--- are-similar.js
+++ are-similar.js
@@ -1,22 +1,22 @@
 function solution(a, b) {
-  let result = true;
-  let aVar = null;
-  let bVar = null;
-  let swapped = false;
+  let arrOne = [];
+  let arrTwo = [];
+  if (a.join() === b.join()) {
+    return true;
+  }
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
-      if (aVar === null || bVar === null) {
-        aVar = a[i];
-        bVar = b[i];
-      } else {
-        if (swapped || aVar !== b[i] || bVar !== a[i]) {
-          result = false;
-        }
-        swapped = true;
-      }
+      arrOne.push(a[i]);
+      arrTwo.push(b[i]);
     }
   }
-  return result;
+
+  arrTwo = arrTwo.reverse().join();
+  arrOne = arrOne.join();
+  if (arrOne === arrTwo) {
+    return true;
+  }
+  return false;
 }
 
 solution();
